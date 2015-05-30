35ac79397f26cebbea7d210298816a70d6d76da6
George Green <george@theswift.guru>
2015-05-30 22:41:02 +0100

    Created empty project.

---
+++ DragonLog/DragonLog/DragonLog.h
@@ -0,0 +1,19 @@
+//
+//  DragonLog.h
+//  DragonLog
+//
+//  Created by George Green on 30/05/2015.
+//  Copyright (c) 2015 George Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DragonLog.
+FOUNDATION_EXPORT double DragonLogVersionNumber;
+
+//! Project version string for DragonLog.
+FOUNDATION_EXPORT const unsigned char DragonLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DragonLog/PublicHeader.h>
+
+
