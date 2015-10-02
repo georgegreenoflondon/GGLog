d9afc546358842453125381168ec0426321794a3
George Green <george@theswift.guru>
2015-10-02 20:10:01 +0100

    More renaming.

---
+++ GGLog/GGLog/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GGLog
+//
+//  Created by George Green on 30/05/2015.
+//  Copyright (c) 2015 George Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+}
+
